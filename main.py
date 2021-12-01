a60b444bc09d51f10848ae670b38ba5ca12ec0ca
bvac30 <92435924+bvac30@users.noreply.github.com>
2021-12-01 12:11:30 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+#Python Flask aplikacija
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/kontakt")
+def kontakt():
+    return render_template("kontakt.html")
+
+@app.route("/O nama")
+def O_nama():
+    return "Ovo je o nama!"
+
+if __name__ == "__main__":
+    app.run(use_reloader=True)
